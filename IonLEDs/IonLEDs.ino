190cfe0b026d0636f797e1719977b1d30d7a16e5
GrimWreeper <pfoley01@gmail.com>
2020-08-31 18:20:05 +0100

    Improve startup animation for Ion Arm

--- IonLEDs/IonLEDs.ino
+++ IonLEDs/IonLEDs.ino
@@ -4,12 +4,25 @@ const int lowestPin = 2;//the lowest one attach to
 const int highestPin = 13;//the highest one attach to
 
 /**************************************/
-void
-setup() //initialize all of the IO pins
+void setup() //initialize all of the IO pins
 {
   for (int thisPin = lowestPin; thisPin <= highestPin; thisPin++)
   {
     pinMode(thisPin, OUTPUT);
+  }
+
+  for (int ionHeight = lowestPin; ionHeight <= highestPin; ionHeight++)
+  {
+    for (int thisPin = highestPin; thisPin >= lowestPin; thisPin--)
+    {
+      if (thisPin > ionHeight)
+      {
+        digitalWrite(thisPin, HIGH); //turn this led on
+        delay(80);
+        digitalWrite(thisPin, LOW);
+      }
+    }
+    digitalWrite(ionHeight, HIGH);
   }
 }
 /****************************************/
